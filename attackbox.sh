f338d33c6fc0dea920d1257fffdbe93531ff8baf
Perryman <7044121+Perryman@users.noreply.github.com>
2022-09-02 10:11:26 -0400

    Update attackbox.sh
    Add fzf to installation along with running a new shell.

--- attackbox.sh
+++ attackbox.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+
+### TO USE RUN THIS COMMAND:
+### curl https://github.com/Perryman/dotfiles/blob/f0372d4cb6f52a5586df336fd85837a7303e4d66/attackbox.sh && chmod +x attackbox.sh && attackbox.sh
+
+# Install fzf
+git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
+~/.fzf/install
+
+
 # Install stegoveritas
 pip install stegoveritas
 # Get some dependencies...  This comes with the python package
@@ -12,3 +21,6 @@ sudo apt -y install mediainfo
 
 # Install zsteg from ruby gems
 gem install zsteg
+
+### Might need to restart the shell after this to make sure paths are up to date and stuff. and rc files.
+/bin/bash
